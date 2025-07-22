168e09d6b721ea8ba99081ce408324b33f55e2d8
HenriqueBragaMoreira <shenrique40moreira@gmail.com>
2025-07-21 21:46:04 -0300

    feat(web): initialize we-finance web app

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import "@/styles/globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"], display: "swap" });
+
+export const metadata: Metadata = {
+  title: "We Finance",
+  description: "We Finance - Your Personal Finance App",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={`${inter.className} font-sans antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
