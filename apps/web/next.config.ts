a205b71b3ac80af2188a8c7d3a6d377445b165d3
Henrique Braga <shenrique40moreira@gmail.com>
2025-09-15 11:01:25 -0300

    chore(deps): downgrade better-auth to version 1.3.9 in package.json and pnpm-lock.yaml

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  rewrites: async () => {
+    return {
+      beforeFiles: [
+        // api/auth to backend_url
+        {
+          source: "/api/:path*",
+          destination: new URL(
+            "/api/:path*",
+            process.env.NEXT_PUBLIC_API_URL
+          ).toString(),
+        },
+      ],
+      afterFiles: [],
+      fallback: [],
+    };
+  },
   redirects: async () => {
     return [
       {
